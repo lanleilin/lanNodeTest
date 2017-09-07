59b751c34dcb29f1c4eb8cf43a1890905a55d335
lanleilin <2476081528@qq.com>
2017-09-08 01:33:27 +0800

    feature

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 // var name = 'world';
 
-// var s = `hello,$(name)!`;
+// var s = `hello,${name}!`;
 
 // console.log(s);
 
@@ -10,3 +10,14 @@ var name = 'Node.js';
 var s = `Hello, ${name}!`;
 
 console.log(s);
+
+var fs = require('fs');
+
+var data = 'Hello, Node.js';
+fs.writeFile('output.txt', data, function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('ok.');
+    }
+});
