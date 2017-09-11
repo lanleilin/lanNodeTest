498719d8eb01caa14a2786f9b7de8346440c2678
lanleilin <2476081528@qq.com>
2017-09-12 01:28:00 +0800

    post

--- lanKoa/controllers/api.js
+++ lanKoa/controllers/api.js
@@ -15,5 +15,14 @@ module.exports = {
         ctx.response.body = {
             products: products
         };
+    },
+    'POST /api/products': async(ctx, next) => {
+        var p = {
+            name: ctx.request.body.name,
+            price: ctx.request.body.price
+        };
+        products.push(p);
+        ctx.response.type = 'application/json';
+        ctx.response.body = p;
     }
 }
